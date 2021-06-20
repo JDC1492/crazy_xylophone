9bbc207af05de620639241ab5fce5fe87ed200bc
JDC1492 <Jonathan_Cohen10@yahoo.com>
2021-06-20 15:47:08 -0400

    Day 2 Complete, Practiced appending text box information to an area on the HTMLpage

--- everyday.js
+++ everyday.js
@@ -1,5 +1,29 @@
 const button = document.getElementById("blue");
-
+const form = document.querySelector(".party-form");
+const party = document.querySelector(".party");
+//First
 button.addEventListener("click", () => {
   document.body.style.backgroundColor = "#00FF00";
+});
+
+//Second
+const appendToPage = function (a, b, c, d) {
+  return `${a}, will act as the Warrior.</br>
+  ${b}, will act as the Mage.</br>
+  ${c}, will act as the Cleric.</br>
+  ${d}, will act as the Wizard.</br>
+  `;
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let warrior = event.target.warrior.value;
+  let mage = event.target.mage.value;
+  let cleric = event.target.cleric.value;
+  let wizard = event.target.wizard.value;
+  if (warrior && mage && cleric && wizard != "") {
+    party.innerHTML += appendToPage(warrior, mage, cleric, wizard);
+  } else {
+    return (party.innerText += "All job spaces must be occupied to continue");
+  }
 });
