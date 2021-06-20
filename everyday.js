f23005408ef9c365f216121d9593b223efc00e4e
JDC1492 <Jonathan_Cohen10@yahoo.com>
2021-06-20 10:28:41 -0400

    made correction to hexidecimal code for the button. was red, now the correct color of green

--- everyday.js
+++ everyday.js
@@ -1,5 +1,5 @@
 const button = document.getElementById("blue");
 
 button.addEventListener("click", () => {
-  document.body.style.backgroundColor = "#AA0000";
+  document.body.style.backgroundColor = "#00FF00";
 });
